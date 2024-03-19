78d51a8d2a1d30ce4a5619ef775600fba10df638
MuhirsanDev <muhirsan.id@gmail.com>
2024-03-19 22:39:08 +0700

    Operator Assignment

---
+++ 11. Operator Assignment/main.cpp
@@ -0,0 +1,37 @@
+/*
+
+Operator assignment dalam C++ digunakan untuk memberikan nilai kepada sebuah variabel. Operator assignment digunakan untuk menginisialisasi variabel dengan suatu nilai atau untuk memperbarui nilai variabel dengan nilai baru. Operator assignment ditandai dengan simbol "=".
+
+Penjelasan singkat operator assignment:
+
+- "=" adalah operator assignment dasar yang digunakan untuk memberikan nilai kepada variabel.
+- Misalnya, dalam pernyataan "x = 5;", operator assignment "=" digunakan untuk memberikan nilai 5 kepada variabel x.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+
+    // Menggunakan operator assignment untuk memberikan nilai 5 kepada variabel x
+    x = 5;
+
+    // Mencetak nilai variabel x
+    cout << "Nilai variabel x: " << x << endl;
+
+    return 0;
+}
+
+/*
+
+Penjelasan singkat program di atas:
+
+1. Program mendeklarasikan sebuah variabel bertipe int bernama x.
+2. Program menggunakan operator assignment "=" untuk memberikan nilai 5 kepada variabel x.
+3. Program mencetak nilai variabel x menggunakan pernyataan "cout".
+4. Output program akan menampilkan "Nilai variabel x: 5".
+
+*/
